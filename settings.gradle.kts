eca37828e1a621e4c6bb1ae9dceefbb2f834e6d6
unknown <NahadAdmin@Cli3-024.Nahad.ir>
2023-11-26 10:48:54 +0330

    [refactor] add countdown module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "TimeBreakWithCompose"
 include(":app",
-    ":library:designsystem")
+    ":library:designsystem",
+    ":feature:countdown",
+)
