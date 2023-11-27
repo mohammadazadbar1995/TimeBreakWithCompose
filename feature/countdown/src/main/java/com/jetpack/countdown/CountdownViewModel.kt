e338af61bbc1570bfba484c2333b0c3cd3744f14
unknown <NahadAdmin@Cli3-024.Nahad.ir>
2023-11-27 08:38:49 +0330

    [feature] add CountdownViewModel

--- feature/countdown/src/main/java/com/jetpack/countdown/CountdownViewModel.kt
+++ feature/countdown/src/main/java/com/jetpack/countdown/CountdownViewModel.kt
@@ -1,4 +1,30 @@
 package com.jetpack.countdown
 
-class CountdownViewModel {
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CountdownViewModel : ViewModel() {
+
+    private val _countdownState = mutableStateOf(CountdownState())
+    val countdownState: State<CountdownState> = _countdownState
+
+
 }
+
+data class CountdownState(
+    val counterState: CounterState = CounterState.INITIAL,
+    val workingState: WorkingState = WorkingState.WORk,
+    val remainTime: Int = REST_DURATION,
+)
+
+enum class CounterState {
+    INITIAL, PLAY, PAUSE
+}
+
+enum class WorkingState {
+    REST, WORk
+}
+
+const val REST_DURATION = 20
+const val WORKING_DURATION = 20 * 60
